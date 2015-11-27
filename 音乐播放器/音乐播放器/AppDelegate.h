3d5c6671a873824624d355ab058923dce97b102b
hanyongwei <m15801642015@163.com>
2015-11-27 04:23:19 -0800

    搭建项目

---
+++ 音乐播放器/音乐播放器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  音乐播放器
+//
+//  Created by mac on 15/11/27.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
